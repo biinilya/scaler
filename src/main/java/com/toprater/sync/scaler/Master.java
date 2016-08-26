bbd38803cabe3f99a49c4ad05e5581de23698b31
dreyk <gunin@satissoft.ru>
2016-08-25 18:40:18 -0700

    Just check

--- src/main/java/com/toprater/sync/scaler/Master.java
+++ src/main/java/com/toprater/sync/scaler/Master.java
@@ -10,13 +10,14 @@ import static spark.Spark.*;
 
 public class Master{
     private final static Logger log = LoggerFactory.getLogger(Master.class);
+
     public static void main(String[] args) throws Exception {
         port(7476);
         Gson gson = new Gson();
         new Thread(new SyncTest()).run();
+        new Thread(new SyncReceiver()).run();
         get("/prepare", (request, response) -> {
             log.info("prepare");
-            new Thread(new SyncReceiver()).run();
             return "";
         });
         get("/started","application/json", (request, response) -> {
